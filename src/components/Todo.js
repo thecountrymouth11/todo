56680500df99dde87d385a1ba5ca0a2870f3ab2f
thecountrymouth11 <thecountrymouth@gmail.com>
2023-04-14 23:20:20 +0530

    displaying-todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 export const Todo = () => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className="Todo">
+      <p>Go for shopping!</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  );
+};
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -17,6 +18,7 @@ export const TodoWrapper = () => {
   return (
     <div className="TodoWrapper">
       <TodoForm addTodo={addTodo} />
+      <Todo />
     </div>
   );
 };
