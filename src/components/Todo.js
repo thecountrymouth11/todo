6a60dbee168be12da334396b2cf7eb0bdc8431cf
thecountrymouth11 <thecountrymouth@gmail.com>
2023-04-16 18:53:24 +0530

    making-todos-complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,10 +2,15 @@ import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
-export const Todo = ({task}) => {
+export const Todo = ({ task, toggleComplete }) => {
   return (
     <div className="Todo">
-      <p>{task.task}</p>
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
         <FontAwesomeIcon icon={faTrash} />
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -15,11 +15,20 @@ export const TodoWrapper = () => {
     console.log(todos);
   };
 
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
   return (
     <div className="TodoWrapper">
+      <h1>Get Things Done!</h1>
       <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} />
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
       ))}
     </div>
   );
